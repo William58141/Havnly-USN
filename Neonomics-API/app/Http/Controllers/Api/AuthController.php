f4547a62e905557b5c505557a9bbe2176124b292
Leander Gangsø <Leander.gangso98@gmail.com>
2022-01-26 12:28:06 +0100

    start fix migration

--- Neonomics-API/app/Http/Controllers/Api/AuthController.php
+++ Neonomics-API/app/Http/Controllers/Api/AuthController.php
@@ -3,42 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Support\Facades\Neonomics;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    private $data;
-    private $data2;
-
-    public function __construct(Request $res)
+    public function index(Request $request)
     {
-
-        $this->data = $res;
-
-        // $this->data = $res->route();
-        // $this->data = $res->isMethod('POST');
-        // $this->data = $res->isMethod('GET');
-        // $this->data = $res->header();
-        // $this->data2 = $res->query();
-        // $this->data = $res->getQueryString();
-        // $this->data = $res->getPort();
-        // $this->data = $res->post();
-        // $this->data = $res->all();
-        // $this->data = $res->has('id');
-
-
-
-
-        // $this->data = Neonomics::test('ok');
-    }
-
-    public function index()
-    {
+        $token = $request->user()->createToken($request->token_name);
         return response()->json([
-            'status' => 'OK',
-            'post' => $this->data,
-            'get' => $this->data2,
+            'ok' => true,
+            'token' => $token->plainTextToken,
         ]);
     }
 }
--- Neonomics-API/routes/api.php
+++ Neonomics-API/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,13 +12,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::post('/auth', [App\Http\Controllers\Api\AuthController::class, 'index']);
+Route::post('/auth', [App\Http\Controllers\Api\AuthController::class, 'index']);
 
 Route::get('/banks', [App\Http\Controllers\Api\BankController::class, 'index']);
 Route::get('/banks/{id}', [App\Http\Controllers\Api\BankController::class, 'show']);
@@ -27,9 +28,8 @@ Route::get('/accounts/{id}', [App\Http\Controllers\Api\AccountController::class,
 Route::get('/accounts/{id}/balances', [App\Http\Controllers\Api\AccountController::class, 'balances']);
 Route::get('/accounts/{id}/transactions', [App\Http\Controllers\Api\AccountController::class, 'transactions']);
 
-
 // catch all 404
-Route::fallback(function() {
+Route::fallback(function () {
     return response()->json([
         'ok' => false,
         'error' => 404,
